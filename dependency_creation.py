d61a312a876e37d73705c63dbd0e11d8f6574f75
Guneet Singh Dhillon <guneet@TBCD7.WPA.Dal.Ca>
2020-02-28 22:45:03 -0400

    factorial added by user 1

---
+++ dependency_creation.py
@@ -0,0 +1,6 @@
+f = open("input.txt", "w")
+for i in range(1,101):
+	#print(str(i))
+	f.write(str(i) + "\n")
+
+f.close()
---
+++ factorial.py
@@ -0,0 +1,19 @@
+import datetime
+import time
+
+f = open("input.txt", "r")
+fo = open("fac_out.csv", "w")
+for num in f:
+	num = num.strip()
+	n = int(num)
+	temp = n
+	fac = 1
+	t1 = datetime.datetime.now()
+	while(temp>=1):
+		fac = fac*temp
+		temp=temp-1
+	t2 = datetime.datetime.now()
+	w = str(num) + "," + str(t2-t1) + "," + str(fac) + "\n"
+	fo.write(w)
+f.close()
+fo.close()
